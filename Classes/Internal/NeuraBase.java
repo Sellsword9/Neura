b703a490988e23129efc8ae32014e5b7a25e397d
Sellsword9 <yerayromeroortiz@gmail.com>
2023-05-30 12:50:49 +0200

    Adding orphan input class

---
+++ Classes/Internal/NeuraBase.java
@@ -0,0 +1,13 @@
+package Classes.Internal;
+
+/**
+ * A NeuraInput with no parent
+ * @see NeuraInput
+ */
+public class NeuraBase extends NeuraInput {
+
+    public NeuraBase(double value) {
+        super(value, null);
+    }
+    
+}
